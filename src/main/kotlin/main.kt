5508d1bfabc3f91698cfe0110d563097520874ea
webpekr <webpekr@gmail.com>
2021-07-01 23:37:51 +0900

    명령어 입력

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,12 @@
 fun main() {
-    println("하이");
+    println("안녕하세요.")
+    println("== 게시판 관리 프로그램 시작 ==")
+
+    while ( true ) {
+        print("명령어) ")
+        readLine()
+        val command = readLine()
+        println("입력한 명령어 : $command")
+    }
+    println("== 게시판 관리 프로그램 끝 ==")
 }
-
-
-
